82c9bfe18b1a8afe6a6733f9a088035a7d6f99d4
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-03 23:26:05 -0300

    Update dados_2.js

--- inicio/dados_2.js
+++ inicio/dados_2.js
@@ -4,3 +4,12 @@ let precoComDesconto = preco * (1-desconto);
 
 
 console.log(precoComDesconto);
+
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log("Produto: " + nome 
+    + " Categoria: " + categoria 
+    + ", Preço: " + preco 
+    + ", Desconto: " + desconto 
+    + ", Preço Final: " + precoComDesconto);
