f18e366b3db974bcff6a4d751c1ca068f7357462
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 14:52:10 -0300

    Update math.js

---
+++ fundamentos/math.js
@@ -0,0 +1,6 @@
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2); // Math é a API de operações matemáticas dentro do Js, O pow utilizado foi para criar a exponenciação
+
+console.log(area.toFixed(2)); // toFixed para deixar a área com apenas duas casas decimais
+
+console.log(typeof Math)
