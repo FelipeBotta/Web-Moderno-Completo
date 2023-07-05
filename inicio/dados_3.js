631a0df4879178d59a287d675d2d6b3d1aa8fa1e
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-04 21:55:56 -0300

    Update dados_3.js

--- inicio/dados_3.js
+++ inicio/dados_3.js
@@ -2,6 +2,12 @@ let idade = 31;
 
 console.log(typeof idade);
 
-let salario = 4568.32;
+let salario = 4578.32;
 
 console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+
+console.log(typeof estaChovendo)
+
+
