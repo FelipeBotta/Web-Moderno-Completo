653f23d85e27f32f05106c5d1dd3c2f1abf47348
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 14:39:26 -0300

    Create numerosAlgunsCuidados.js

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,12 @@
+console.log(7 / 0); // retornara infinito
+
+console.log("10" / 2); // em JS string pode ser convertida automaticamente para realizar a operação
+
+console.log("Show !" * 2); // erro pois show não é um número (NaN = Not a Number)
+
+console.log(0.1 + 0.7); // não sera preciso o resultado sera 0.799999
+
+// console.log(10.toString()); não é possivel converter um número inteiro para string, apenas se utilizar a função Number
+
+console.log((10.345).toFixed(2)); // toFixed utilizado para delimitarmos a quantidade de casas decimais após a vírgula
+
