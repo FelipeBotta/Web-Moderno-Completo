2d761537029122b4288f3863a179f575ee66dd38
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 16:24:18 -0300

    Update numerosAlgunsCuidados.js

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -10,3 +10,6 @@ console.log(0.1 + 0.7); // não sera preciso o resultado sera 0.799999
 
 console.log((10.345).toFixed(2)); // toFixed utilizado para delimitarmos a quantidade de casas decimais após a vírgula
 
+console.log("3" + 2); // como tem logica concatenar string com numero o Js executa uma concatenação formando 32
+
+console.log("3" - 2); // nesse caso como não faz sentindo juntar igual na soma, transforma a string em numero e subtrai
