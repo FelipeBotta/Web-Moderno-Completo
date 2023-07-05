16035f4ff06f851656ac1177c658eeea8439d808
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 18:49:08 -0300

    Update nullUndefined.js

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,24 @@
+let valor; // não inicializada
+console.log(valor);
+
+valor = null; // ausencia de valor
+console.log(valor);
+// console.log(valor.toString()); // Erro !
+
+// se precisar zerar o valor de alguma variavel utilizar o null
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.preco = 3.50;
+console.log(produto)
+
+produto.preco = undefined;
+console.log(!!produto.preco);
+// delete produto.preco; (caso quisesse excluir o preço do produto)
+console.log(produto);
+
+produto.preco = null; // sem preço
+console.log(!!produto.preco);
+console.log(produto);
