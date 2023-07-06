6e1965df8014c1cddadef03d88634356d845bb2d
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 23:46:48 -0300

    Create hoisting.js

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,10 @@
+console.log("a =", a);
+var a = 2;
+console.log("a =", a);
+
+console.log("b =",b);
+let b = 2;
+console.log("b =", b);
+
+
+// hoisting basicamente é algo que o Js faz para jogar a variavel var para cima, como no exemplo do var onde não existia variavel na primeira impressão e entre a primeira e a segunda foi declarada a var a e por conta disso vem o hoisting e faz com que a primeira impressão seja undefined e a segunda já com o valor atribuido. Em outras linguagens daria erro por não haver a variavel como também acontece quando utilizamos o let ao inves do var.
