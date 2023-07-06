4f0b1bb32dde633e55e1c7f6f54466126a9fb31a
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 22:28:15 -0300

    Create usandoVar1.js

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,22 @@
+{
+     {
+         { 
+            { 
+                var sera = "Será???" 
+}
+ }
+ } 
+}
+
+console.log(sera)
+
+
+function teste (){
+    var local = 123
+}
+
+
+// se a variavel var não estiver dentro de uma função ela consegue ser impressa dentro de qualquer bloco de código, porém, se a variavel estiver dentro de uma função ela fica dentro dela apenas.
+
+// variavel tem a possibilidade de ser global ou no escopo da função (global pode ocasionar problema caso alguem altere o código com uma variavel com o mesmo nome)
+
