658481d787c76054ab9f9a552ee1065146a83e5d
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-05 23:19:24 -0300

    Update usandoVarEmLoop2.js

--- fundamentos/usandoVarEmLoop2.js
+++ fundamentos/usandoVarEmLoop2.js
@@ -8,3 +8,5 @@ for (var i = 0 ; i < 10 ; i++){
 
 funcs[2]()
 funcs[8]()
+
+// o var por não ter escopo de função imprime duas vezes o último valor indepente de termos solicitados o item da posição 2 e 8
