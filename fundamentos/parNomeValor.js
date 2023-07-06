1d69d556014948d732c211d059cc47ba01801177
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-06 16:39:23 -0300

    Update parNomeValor.js

--- fundamentos/parNomeValor.js
+++ fundamentos/parNomeValor.js
@@ -2,7 +2,7 @@
 
 const saudacao = "opa"; // contexto léxico 1
 
-function exec(){
+function exec() {
     const saudacao = "Fala" // contexto léxico 2
     return saudacao
 }
@@ -13,9 +13,9 @@ const cliente = {
     nome: "Pedro",
     idade: 32,
     peso: 90,
-    endereco:{
+    endereco: {
         logradouro: "Rua X",
-        numero: 123 
+        numero: 123
     }
 }
 
@@ -24,4 +24,6 @@ console.log(exec());
 console.log(cliente);
 
 
-// Neste caso se tentassemos redeclarar a saudação daria erro pois é uma constante, se fosse var ele reatribuiria com o último valor recebio, mais um ponto para optar por const ou let e não var
+// Neste caso se tentassemos redeclarar a saudação daria erro pois é uma constante, se fosse var ele reatribuiria com o último valor recebio, mais um ponto para optar por const ou let e não var.
+
+// No exemplo também as saudações não se conflitam por estarem em níveis diferentes, uma declarada como uma constante no escopo global e a outra dentro de uma função ou seja dentro de um bloco especifico.
