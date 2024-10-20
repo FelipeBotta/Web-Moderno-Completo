dbec8bb14c958143aa8a8fdeb6873c175a2e5f0e
Luis Felipe S Botta <felipeex7@gmail.com>
2024-10-20 11:26:09 -0300

    -

---
+++ funcao/thisEBind.js
@@ -0,0 +1,16 @@
+const pessoa = {
+  saudacao: 'Bom dia !',
+  falar(){
+    console.log(this.saudacao)
+  }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // Conflito entre paradigmas: Funcional e Orientado a Objetos
+
+const falarDePessoa = pessoa.falar.bind(pessoa) // Bind faz o link ao objeto
+falarDePessoa() 
+
+const falar2 = pessoa.falar
+falar2()
