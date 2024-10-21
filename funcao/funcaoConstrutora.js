4a0304087973bfe7de0515ed6abe93ca1f7b72d7
Luis Felipe S Botta <felipeex7@gmail.com>
2024-10-20 23:09:29 -0300

    -

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,39 @@
+function Carro(velocidadeMaxima = 200, delta = 5){
+  // atributo privado
+  let velocidadeAtual = 0
+
+  // metodo publico
+  this.acelerar = function () {
+    if (velocidadeAtual + delta <= velocidadeMaxima){
+      velocidadeAtual += delta
+    } else {
+      velocidadeAtual = velocidadeMaxima
+    }
+  }
+
+  this.frear = function(){
+    if (velocidadeAtual - delta >= 0){
+      velocidadeAtual -= delta
+    } else{
+      velocidadeAtual == 0
+    }
+  }
+
+  // metodo publico
+  this.getVelocidadeAtual = function(){
+    return velocidadeAtual
+  }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350,50)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+ferrari.frear()
+console.log(ferrari.getVelocidadeAtual())
