045a0a6b77b789f2cf2501d79736fe30e2424f1b
Luis Felipe S Botta <felipeex7@gmail.com>
2024-11-06 21:36:28 -0300

    Update forEach.js

---
+++ array/forEach.js
@@ -0,0 +1,11 @@
+// Parametros para o ForEach Nome, Indice e Array (todos opcionais)
+const aprovados = ['Agata','Aldo','Daniel','Raquel']
+
+aprovados.forEach(function(nome, indice) {
+  console.log(`${indice + 1}) ${nome} `)
+} )
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
