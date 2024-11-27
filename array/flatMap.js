efcf31cbbd038f1de863a07fec18b2269e3fdab1
Luis Felipe S Botta <felipeex7@gmail.com>
2024-11-26 21:24:16 -0300

    Update flatMap.js

--- array/flatMap.js
+++ array/flatMap.js
@@ -22,3 +22,12 @@ const getNotasTurma = turma => turma.alunos.map(getNotaAluno)
 
 const notas1 = escola.map(getNotasTurma)
 console.log(notas1)
+
+console.log([].concat([8.1, 9.3], [8.9 , 7.3]))
+
+Array.prototype.flatMap = function(callback){
+  return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasTurma)
+console.log(notas2)
